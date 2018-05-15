6688beeba817b60190fda3aeea60ba75c82fc73b
刘向东 <liuxiangdong001@lianjia.com>
2018-05-15 14:21:50 +0800

    trans test

--- app.js
+++ app.js
@@ -8,38 +8,28 @@ const CONFIG = {
     url: 'https://translate.google.cn/#en/zh-CN/',
 };
 
-let filePath = path.join(__dirname,'test.txt');
+let filePath = path.join(__dirname,CONFIG.file);
 
-async function read(filePath) {
-    try {
-        const fr = await readFile(filePath,"utf-8");
-        return fr;
-    } catch (err) {
-        console.log('Error', err);
-    }
-}
-
-async function run(origin) {
-    const browser = await puppeteer.launch();
+(async () => {
+    const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    const url = CONFIG.url;
-    let str = encodeURIComponent(origin);
-    await page.goto(url+str);
-    const USER_SELECTOR = '#result_box > span';
-    let result = await page.evaluate((sel) => {
-        return document.querySelector(sel).innerHTML;
-    }, USER_SELECTOR);
+    await page.goto(CONFIG.url,{waitUntil: 'networkidle2'});
+    let fr = await readFile(filePath,"utf-8");
+    const tmpArr = fr.split('\n');
+
+    for(let i=0; i<tmpArr.length; i++){
+        await page.type('#source', tmpArr[i], {delay: 100});
+        page.click('#src-translit');
+        await page.waitFor(1000);
+        let targetLink = await page.evaluate(() => {
+            let text =  document.querySelector('#result_box span').innerText;
+            return text;
+        });
+        console.log(i,tmpArr[i]);
+        console.log(targetLink);
+        page.click('#gt-clear');
+        await page.waitFor(1000);
+    }
+    await page.waitFor(1000);
     browser.close();
-    return result;
-}
-
-async function main(){
-    let test = await read(filePath);
-    const tmpArr = test.split('\n');
-    tmpArr.forEach(async value =>{
-        let res = await run(value)
-        console.log(res);
-    })
-}
-
-main();
+})();
