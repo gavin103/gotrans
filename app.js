c22cd55727886ae79d67eef90880c4270e7fb19d
刘向东 <liuxiangdong001@lianjia.com>
2018-05-19 15:09:41 +0800

    read and write

--- app.js
+++ app.js
@@ -1,35 +1,60 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 const fs = require('fs');
-const readFile = require("util").promisify(fs.readFile);
+const util = require('util');
+
+const appendFile = util.promisify(fs.appendFile);
 
 const CONFIG = {
-    file: 'test.txt',
-    url: 'https://translate.google.cn/#en/zh-CN/',
+    input: 'test.txt',
+    out:'out.txt',
+    url: 'https://translate.google.cn/#en/zh-CN/'
 };
 
-let filePath = path.join(__dirname,CONFIG.file);
+const inputPath = path.join(__dirname,CONFIG.input);
+const outPath = path.join(__dirname,CONFIG.out);
 
-(async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto(CONFIG.url,{waitUntil: 'networkidle2'});
-    let fr = await readFile(filePath,"utf-8");
-    const tmpArr = fr.split('\n');
+const trans = async ()=>{
+    try {
+        //打开浏览器，进入谷歌翻译网页
+        const browser = await puppeteer.launch({headless: false});
+        const page = await browser.newPage();
+        await page.goto(CONFIG.url);
 
-    for(let i=0; i<tmpArr.length; i++){
-        await page.type('#source', tmpArr[i], {delay: 100});
-        page.click('#src-translit');
-        await page.waitFor(1000);
-        let targetLink = await page.evaluate(() => {
-            let text =  document.querySelector('#result_box span').innerText;
-            return text;
+        fs.writeFileSync(outPath,'');
+        //读取文档
+        let fr = fs.readFileSync(inputPath,"utf-8");
+        let tmpArr = [];
+        fr.split('\n').forEach((line,index)=>{
+            let tmp = line.trim();
+            tmp.length > 0 && tmpArr.push(' '+tmp)
         });
-        console.log(i,tmpArr[i]);
-        console.log(targetLink);
-        page.click('#gt-clear');
+
+        for(let i=0; i<tmpArr.length; i++){
+            let tmpInput = tmpArr[i];
+            await page.type('#source', tmpInput,{delay: 10});
+            page.click('#src-translit');
+            await page.waitFor(1500);
+            let transOutput = await page.evaluate(() => {
+                let text='';
+                if(document.querySelector('#result_box span')){
+                    [...document.querySelectorAll('#result_box span')].forEach((nod)=>{
+                        text+=nod.innerText;
+                    })
+                }
+                return text;
+            });
+            await appendFile(outPath,tmpInput+'\n');
+            await appendFile(outPath,transOutput+'\n');
+            await page.waitFor(500);
+            page.click('#gt-clear');
+        }
         await page.waitFor(1000);
+        browser.close();
+    }catch (e) {
+        console.log(e);
     }
-    await page.waitFor(1000);
-    browser.close();
-})();
+
+};
+
+trans();
